c374415a04f358bd337f2c8cad693257d4d9b8f8
Hizuro <hizuro@gmx.net>
2021-05-10 06:04:37 +0200

    minor changes in localization file

--- localizations.lua
+++ localizations.lua
@@ -16,64 +16,9 @@ ns.L = L;
 --L["Custom"] = VIDEO_QUALITY_LABEL6; -- Custom
 L[addon] = "AFK Fullscreen Warning";
 
---@do-not-package@
-L["AddOnLoaded"] = "AddOn loaded..."
-L["AddOnLoadedDesc"] = "Show 'AddOn loaded...' message on login"
-L["ShowDemo"] = "Show demo"
-L["ShowDemoDesc"] = "Display a little demo frame to show a selected skin"
-L["HideUI"] = "Hide UI"
-L["HideUIDesc"] = "Hide user interface on AFK"
-L["FullscreenTexture"] = "Fullscreen texture"
-L["FullscreenTextureDesc"] = "Choose texture for fullscreen warning"
-L["ViewportSupport"] = "Viewport support"
-L["ViewportSupportDesc"] = "Anchor all elements on WorldFrame instead of UIParent. Usefull for AddOns like CT_Viewport from CTMod."
---@end-do-not-package@
-
 -- @localization(locale="enUS", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
 
 if LOCALE_deDE then
---@do-not-package@
-	L["AFK since"] = "AFK seit" -- AFK_fullscreen.lua
-	L["Alert sound options"] = "Alarmton Optionen"; -- options.lua
-	L["Border blizzard like"] = "Umrandung Blizzard ähnlich"; -- options.lua
-	L["Bottom"] = "Unten"; -- options.lua
-	L["Click"] = "Klick"; -- AFK_fullscreen.lua
-	L["Clock animation"] = "Uhranimation"; -- options.lua
-	L["Dancing"] = "Tanzend"; -- options.lua
-	L["Do nothing"] = "Tue nichts"; -- options.lua
-	L["Enable alert sound"] = "Aktiviere Alarmton"; -- options.lua
-	L["File"] = "Datei"; -- options.lua
-	L["Flat"] = "Flach"; -- options.lua
-	L["Fullscreen options"] = "Vollbild Optionen"; -- options.lua
-	L["FullscreenTextureDesc"] = ""; -- options.lua
-	L["FullscreenTexture"] = "Vollbild Grafik"; -- options.lua
-	L["Fullscreen warning"] = "Vollbildwarnung"; -- options.lua
-	L["Gradient centered horizontal"] = "Verlauf zentriert Honrizontal"; -- options.lua
-	L["Gradient centered vertical"] = "Verlauf zentriert Vertikal"; -- options.lua
-	L["HideUIDesc"] = "Verstecke die Benutzeroberfläche bei AFK"; -- options.lua
-	L["HideUI"] = "Verstecke UI"; -- options.lua
-	L["Info panel elements"] = "Infopanel Elemente"; -- options.lua
-	L["Info panel skins"] = "Infopanel Aussehen"; -- options.lua
-	L["Left click"] = "Links Klick"; -- AFK_fullscreen.lua
-	L["Left"] = "Links"; -- options.lua
-	L["Middle"] = "Mitte"; -- options.lua
-	L["Minimap button"] = "Minikarten Button"; -- options.lua
-	L["Panel position"] = "Panel Position"; -- options.lua
-	L["Player name & realm"] = "Spielername & Realm"; -- options.lua
-	L["Player portrait"] = "Spielerportrait"; -- options.lua
-	L["Random"] = "Zufall"; -- options.lua
-	L["Reset color"] = "Farbe zurücksetzen"; -- options.lua
-	L["Right"] = "Rechts"; -- options.lua
-	L["Select alert sound"] = "Wähle Alarmton"; -- options.lua
-	L["Select a skin"] = "Wähle ein Aussehen"; -- options.lua
-	L["Select output channel"] = "Wähle Ausgabekanal"; -- options.lua
-	L["ShowDemo"] = "Zeige Demo"; -- options.lua
-	L["ShowDemoDesc"] = "Zeigt ein kleines Demofenster zu anzeigen des gewählten Skin"; -- options.lua
-	L["Time & date"] = "Uhrzeit & Datum"; -- options.lua
-	L["to open config"] = "zum öffnen der Einstellungen"; -- AFK_fullscreen.lua
-	L["Top"] = "Oben"; -- options.lua
-	L["Use custom file"] = "Nutze eigene Datei"; -- options.lua
---@end-do-not-package@
 --@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
 end
 
